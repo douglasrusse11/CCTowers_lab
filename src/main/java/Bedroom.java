69c0ed8fe58117b6f57f204098c59ced3afd2b74
Douglas Russell <douglas.russell7@btinternet.com>
2022-01-13 14:04:46 +0000

    Changes by Michael

--- src/main/java/Bedroom.java
+++ src/main/java/Bedroom.java
@@ -3,10 +3,11 @@ public class Bedroom extends Room {
     private int roomNumber;
     private RoomType roomType;
 
-    public Bedroom(int _capacity, int _roomNumber, RoomType _roomType) {
+    public Bedroom(int _capacity, int _roomNumber, RoomType _roomType, double nightlyRate) {
         super(_capacity);
         this.roomNumber = _roomNumber;
         this.roomType = _roomType;
+        this.nightlyRate = nightlyRate;
     }
 
     public int getRoomNumber() {
